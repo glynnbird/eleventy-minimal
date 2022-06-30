66a27a70fcea46e4a788a15b6f4140e6c2af4935
Glynn Bird <glynn.bird@gmail.com>
2022-06-30 10:22:52 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,13 @@ module.exports = function(eleventyConfig) {
   // copy static assets straight through to final site
   eleventyConfig.addPassthroughCopy('assets')
 
-
   // if you're using a custom domain, add the domain name
   // to a CNAME file at the root of your project and
   // uncomment this line. This will ensure that GitHub Pages
   // knows which custom domain to use with your site
   // eleventyConfig.addPassthroughCopy('CNAME')
+  
+  return {
+    pathPrefix: "/eleventy-minimal/" // this is the path preprended to links for GH Pages. Omit for sites deployed to root directory
+  }
 };
